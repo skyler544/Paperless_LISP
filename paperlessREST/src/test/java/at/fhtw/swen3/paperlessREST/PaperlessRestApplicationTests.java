c048a83b80c8b085239bd9392692069dcda38938
Skyler Mayfield <skyler544@gmail.com>
2023-10-13 13:41:55 +0200

    initial commit for web backend

---
+++ paperlessREST/src/test/java/at/fhtw/swen3/paperlessREST/PaperlessRestApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.swen3.paperlessREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
