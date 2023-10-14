3ca8a9d2a18c3b3084584d021e82e4912ef8d1d1
juliob <juliobampi97@gmail.com>
2023-10-14 14:28:08 +0200

    Added dummy test for CI/CD

--- paperlessREST/src/test/java/at/fhtw/swen3/paperlessREST/PaperlessRestApplicationTests.java
+++ paperlessREST/src/test/java/at/fhtw/swen3/paperlessREST/PaperlessRestApplicationTests.java
@@ -3,11 +3,23 @@ package at.fhtw.swen3.paperlessREST;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class PaperlessRestApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void dummyTest() {
+		final int one = 1;
+		final int two = 2;
+
+		final int sum = one + two;
+
+		assertEquals(one + two, sum);
 	}
 
 }
