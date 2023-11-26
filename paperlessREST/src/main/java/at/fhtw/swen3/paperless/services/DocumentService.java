3c2fc7be2ff95bb4629821eff2dd34ee69e8f854
juliob <juliobampi97@gmail.com>
2023-11-26 21:38:30 +0100

    Added logging to DocumentService

--- paperlessREST/src/main/java/at/fhtw/swen3/paperless/services/DocumentService.java
+++ paperlessREST/src/main/java/at/fhtw/swen3/paperless/services/DocumentService.java
@@ -4,10 +4,14 @@ import at.fhtw.swen3.paperless.models.entity.DocumentEntity;
 import at.fhtw.swen3.paperless.repositories.DocumentRepository;
 import at.fhtw.swen3.paperless.services.customDTOs.PostDocumentRequestDto;
 import at.fhtw.swen3.paperless.services.mapper.PostDocumentMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DocumentService {
+
+    Logger logger = LogManager.getLogger(DocumentService.class);
 
     private final DocumentRepository documentRepository;
 
@@ -16,8 +20,17 @@ public class DocumentService {
     }
 
     public DocumentEntity saveDocument(PostDocumentRequestDto postDocumentRequestDto) {
-        var result = PostDocumentMapper.INSTANCE.dtoToEntity(postDocumentRequestDto);
-        documentRepository.save(result);
-        return result;
+
+        try {
+            var result = PostDocumentMapper.INSTANCE.dtoToEntity(postDocumentRequestDto);
+            documentRepository.save(result);
+            return result;
+        } catch (Exception e) {
+
+            this.logger.error(String.format("Error saving document \n%s", e));
+            throw e;
+
+        }
+
     }
 }
