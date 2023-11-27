7e04ce9efe8bc6a4807a6f2dfa99191d17450c00
Peter Gamsjäger <if21b016@technikum-wien.at>
2023-11-27 12:43:34 +0100

    create MQService and corresponding queue configuration

--- paperlessREST/src/main/java/at/fhtw/swen3/paperless/services/DocumentService.java
+++ paperlessREST/src/main/java/at/fhtw/swen3/paperless/services/DocumentService.java
@@ -4,6 +4,7 @@ import at.fhtw.swen3.paperless.models.entity.DocumentEntity;
 import at.fhtw.swen3.paperless.repositories.DocumentRepository;
 import at.fhtw.swen3.paperless.services.customDTOs.PostDocumentRequestDto;
 import at.fhtw.swen3.paperless.services.mapper.PostDocumentMapper;
+import at.fhtw.swen3.paperless.services.messageQueue.MQService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,11 @@ public class DocumentService {
 
     private final DocumentRepository documentRepository;
 
-    public DocumentService(DocumentRepository documentRepository) {
+    private final MQService mqService;
+
+    public DocumentService(DocumentRepository documentRepository, MQService mqService) {
         this.documentRepository = documentRepository;
+        this.mqService = mqService;
     }
 
     public DocumentEntity saveDocument(PostDocumentRequestDto postDocumentRequestDto) {
@@ -24,6 +28,7 @@ public class DocumentService {
         try {
             var result = PostDocumentMapper.INSTANCE.dtoToEntity(postDocumentRequestDto);
             documentRepository.save(result);
+            mqService.processMessage(String.format("Save document with title %s", postDocumentRequestDto.getTitle()));
             return result;
         } catch (Exception e) {
 
