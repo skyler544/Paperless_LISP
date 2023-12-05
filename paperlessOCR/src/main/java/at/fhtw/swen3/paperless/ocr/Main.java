4b038187c668892a3dc7dc61d872adcce0080020
Skyler Mayfield <skyler544@gmail.com>
2023-12-05 12:34:56 +0100

    create project skeleton for OCR worker

---
+++ paperlessOCR/src/main/java/at/fhtw/swen3/paperless/ocr/Main.java
@@ -0,0 +1,9 @@
+package at.fhtw.swen3.paperless.ocr;
+
+import at.fhtw.swen3.paperless.ocr.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ paperlessOCR/src/main/java/at/fhtw/swen3/paperless/ocr/config/ProjectConfig.java
@@ -0,0 +1,9 @@
+package at.fhtw.swen3.paperless.ocr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "at.fhtw.swen3.paperless.ocr")
+public class ProjectConfig {
+}
