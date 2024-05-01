60fbc9e6cb11292d27acde0532cd02c2e6f945b1
Skyler Mayfield <skyler544@gmail.com>
2024-05-01 16:46:22 +0200

    configure minio with application.properties

--- paperlessREST/src/main/java/at/fhtw/swen3/paperless/services/minio/MinioService.java
+++ paperlessREST/src/main/java/at/fhtw/swen3/paperless/services/minio/MinioService.java
@@ -4,9 +4,12 @@ import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+
 import lombok.Getter;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,19 +22,31 @@ public class MinioService implements DocumentStoreService {
 
     private final String BUCKET_NAME = "documents";
 
-    // should not be modified, therefore final and directly initialized.
-    // TODO maybe make a config class and inject it in the constructor instead
+    @Value("${paperless.minio.username}")
+    private String minioUsername;
+
+    @Value("${paperless.minio.password}")
+    private String minioPassword;
+
+    @Value("${paperless.minio.host}")
+    private String minioHostname;
+
+    @Value("${paperless.minio.port}")
+    private int minioPort;
+
     @Getter
     private final MinioClient minioClient =
-        MinioClient.builder().endpoint("http://minio:9000")
-            .credentials("paperless_minio", "paperless").build();
+            MinioClient.builder()
+                    .endpoint("http://" + minioHostname + ":" + minioPort)
+                    .credentials(minioUsername, minioPassword)
+                    .build();
 
     public MinioService() {
         // how many of these logging statements is too many? great logger by the way @Billojullo
         this.logger.info(String.format("Connecting to MinIO\n"));
         try {
             if (!this.minioClient.bucketExists(
-                BucketExistsArgs.builder().bucket(BUCKET_NAME).build()))
+                    BucketExistsArgs.builder().bucket(BUCKET_NAME).build()))
                 this.createBucket(BUCKET_NAME);
         } catch (Exception e) {
             this.logger.error(String.format("Error initializing MinIO \n%s", e));
@@ -56,19 +71,21 @@ public class MinioService implements DocumentStoreService {
             }
 
             try (InputStream inputStream = document.getInputStream()) {
-                minioClient.putObject(PutObjectArgs
-                    .builder()
-                    .bucket(BUCKET_NAME)
-                    .object(document.getOriginalFilename())
-                    .stream(inputStream, document.getSize(), -1).build());
+                minioClient.putObject(
+                        PutObjectArgs.builder()
+                                .bucket(BUCKET_NAME)
+                                .object(document.getOriginalFilename())
+                                .stream(inputStream, document.getSize(), -1)
+                                .build());
             }
 
-            this.logger.info(String.format("Uploading document to MinIO: %s\n",
-                document.getOriginalFilename()));
+            this.logger.info(
+                    String.format(
+                            "Uploading document to MinIO: %s\n", document.getOriginalFilename()));
         } catch (Exception e) {
-            this.logger.error(String.format("Error placing document: %s\n%s",
-                document.getOriginalFilename(), e));
+            this.logger.error(
+                    String.format(
+                            "Error placing document: %s\n%s", document.getOriginalFilename(), e));
         }
     }
-
 }
