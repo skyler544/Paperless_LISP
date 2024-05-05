006485b0a690747128babf68adce88e3551e1597
Skyler Mayfield <skyler544@gmail.com>
2024-05-05 10:36:25 +0200

    remove unused methods

--- paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/ConfigApi.java
+++ paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/ConfigApi.java
@@ -15,18 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.NativeWebRequest;
-
-import java.util.Optional;
 
 @Validated
 @Controller
 public interface ConfigApi {
-
-    default Optional<NativeWebRequest> getRequest() {
-        return Optional.empty();
-    }
-
     @RequestMapping(
             method = RequestMethod.POST,
             value = "/api/ui_settings/",
--- paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/ConfigApiController.java
+++ paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/ConfigApiController.java
@@ -7,14 +7,10 @@ import at.fhtw.swen3.paperless.services.dto.Http200Response;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.NativeWebRequest;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("${openapi.paperlessRestServer.base-path:}")
@@ -25,18 +21,6 @@ public class ConfigApiController implements ConfigApi, BaseLoggingController {
     @Override
     public Logger getLogger() {
         return this.logger;
-    }
-
-    private final NativeWebRequest request;
-
-    @Autowired
-    public ConfigApiController(NativeWebRequest request) {
-        this.request = request;
-    }
-
-    @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return Optional.ofNullable(request);
     }
 
     @Override
