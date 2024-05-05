158a6988ec0865c0065fb7ac991889118f91cbad
Skyler Mayfield <skyler544@gmail.com>
2024-05-05 15:57:54 +0200

    clean up files

--- paperlessREST/src/main/java/at/fhtw/swen3/paperless/services/customDTOs/PostDocumentRequestDto.java
+++ paperlessREST/src/main/java/at/fhtw/swen3/paperless/services/customDTOs/PostDocumentRequestDto.java
@@ -1,8 +1,6 @@
 package at.fhtw.swen3.paperless.services.customDTOs;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.OffsetDateTime;
@@ -10,39 +8,10 @@ import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class PostDocumentRequestDto {
     private String title;
     private OffsetDateTime offsetDateTime;
     private Integer documentType;
     private List<Integer> tags;
     private Integer correspondent;
-
-    public String toString() {
-        return "class PostDocumentRequestDto {\n"
-                + "    title: "
-                + toIndentedString(title)
-                + "\n"
-                + "    offsetDateTime: "
-                + toIndentedString(offsetDateTime)
-                + "\n"
-                + "    documentType: "
-                + toIndentedString(documentType)
-                + "\n"
-                + "    correspondent: "
-                + toIndentedString(correspondent)
-                + "\n"
-                + "    tags: "
-                + toIndentedString(tags)
-                + "\n"
-                + "}";
-    }
-
-    private String toIndentedString(Object o) {
-        if (o == null) {
-            return "null";
-        }
-        return o.toString().replace("\n", "\n    ");
-    }
 }
