7c939e488fedc10c6567ebad6f60b0f529bfc926
Skyler Mayfield <skyler544@gmail.com>
2024-05-05 10:25:01 +0200

    trim search api

--- paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/SearchApiController.java
+++ paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/SearchApiController.java
@@ -1,21 +1,14 @@
 package at.fhtw.swen3.paperless.controller;
 
-
-import jakarta.annotation.Generated;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.NativeWebRequest;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-10-26T19:12:48.175385Z[Etc/UTC]")
 @Controller
 @RequestMapping("${openapi.paperlessRestServer.base-path:}")
 public class SearchApiController implements SearchApi, BaseLoggingController {
@@ -25,18 +18,6 @@ public class SearchApiController implements SearchApi, BaseLoggingController {
     @Override
     public Logger getLogger() {
         return this.logger;
-    }
-
-    private final NativeWebRequest request;
-
-    @Autowired
-    public SearchApiController(NativeWebRequest request) {
-        this.request = request;
-    }
-
-    @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return Optional.ofNullable(request);
     }
 
     @Override
