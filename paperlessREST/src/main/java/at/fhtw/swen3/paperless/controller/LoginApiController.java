aa82383b665b0306d0414d9bd0c121e6a4d2e504
Skyler Mayfield <skyler544@gmail.com>
2024-05-05 10:20:16 +0200

    trim login and tasks api to smallest possible implementation

--- paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/LoginApiController.java
+++ paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/LoginApiController.java
@@ -1,19 +1,14 @@
 package at.fhtw.swen3.paperless.controller;
 
 import at.fhtw.swen3.paperless.services.dto.Statistics200Response;
-import jakarta.annotation.Generated;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.NativeWebRequest;
 
-import java.util.Optional;
-
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-11-03T12:56:46.892911Z[Etc/UTC]")
 @Controller
 @RequestMapping("${openapi.paperlessRestServer.base-path:}")
 public class LoginApiController implements LoginApi, BaseLoggingController {
@@ -23,18 +18,6 @@ public class LoginApiController implements LoginApi, BaseLoggingController {
     @Override
     public Logger getLogger() {
         return this.logger;
-    }
-
-    private final NativeWebRequest request;
-
-    @Autowired
-    public LoginApiController(NativeWebRequest request) {
-        this.request = request;
-    }
-
-    @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return Optional.ofNullable(request);
     }
 
     @Override
@@ -47,6 +30,5 @@ public class LoginApiController implements LoginApi, BaseLoggingController {
         this.logSentResponse("GetStatistics", responseDTO.toString());
 
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
-
     }
 }
--- paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/TasksApiController.java
+++ paperlessREST/src/main/java/at/fhtw/swen3/paperless/controller/TasksApiController.java
@@ -1,25 +1,20 @@
 package at.fhtw.swen3.paperless.controller;
 
-import at.fhtw.swen3.paperless.services.dto.GetTasks200ResponseInner;
-import jakarta.annotation.Generated;
+import at.fhtw.swen3.paperless.services.dto.Http200Response;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.NativeWebRequest;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-11-03T12:56:46.892911Z[Etc/UTC]")
 @Controller
 @RequestMapping("${openapi.paperlessRestServer.base-path:}")
 public class TasksApiController implements TasksApi, BaseLoggingController {
-
     private final Logger logger = LogManager.getLogger(ConfigApiController.class);
 
     @Override
@@ -27,24 +22,11 @@ public class TasksApiController implements TasksApi, BaseLoggingController {
         return this.logger;
     }
 
-    private final NativeWebRequest request;
-
-    @Autowired
-    public TasksApiController(NativeWebRequest request) {
-        this.request = request;
-    }
-
     @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return Optional.ofNullable(request);
-    }
-
-    @Override
-    public ResponseEntity<List<GetTasks200ResponseInner>> getTasks() {
-
+    public ResponseEntity<List<Http200Response>> getTasks() {
         this.logReceivedRequest("GetTasks");
 
-        List<GetTasks200ResponseInner> responseDTO = new ArrayList<GetTasks200ResponseInner>();
+        List<Http200Response> responseDTO = new ArrayList<Http200Response>();
 
         this.logSentResponse("GetTasks", responseDTO.toString());
 
