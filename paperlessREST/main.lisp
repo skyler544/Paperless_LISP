43f91c08d5704fc400e19640733d1205ff03893b
Skyler Mayfield <skyler544@gmail.com>
2024-05-11 21:56:05 +0200

    facilitate interactive development

--- paperlessREST/main.lisp
+++ paperlessREST/main.lisp
@@ -1,8 +1,16 @@
 (defpackage #:paperless-rest/main
   (:nicknames #:paperless-rest/main)
   (:use #:cl)
-  (:import-from #:paperless-rest/router #:handle))
+  (:import-from #:paperless-rest/router #:handle)
+  (:import-from #:clack #:clackup)
+  (:export #:default-server))
 (in-package #:paperless-rest/main)
 
-(lambda (env)
-  (funcall 'handle env))
+(defvar *app*
+  (lambda (env)
+    (funcall 'handle env)))
+
+(defun default-server ()
+  (clackup *app* :port 8080))
+
+*app*
